f893ed868069abafaa26d69bf1fc9dc129354302
Mathenge-G <gmathenge53@gmail.com>
2025-02-19 15:54:31 +0300

    creating pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
 
 const App = () => {
   return (
-    <h1 className="text-3xl text-red-500">
-      Market Mall
-    </h1>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/> 
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+  </BrowserRouter>
   )
 }
 
